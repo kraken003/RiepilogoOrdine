82c4004ffd6da991f4a543beea31347369e80981
kraken003 <kevinsciolti0@gmail.com>
2023-11-24 11:04:05 +0100

    Update Lista prodotti

--- app.js
+++ app.js
@@ -4,17 +4,18 @@ document.addEventListener('DOMContentLoaded', () => {
     const product2 = new Product('2', 'Prodotto 2', ['Variante X', 'Variante Y'], 'Descrizione prodotto 2', 20, 'Retailer 2');
     const product3 = new Product('3', 'Prodotto 3', ['Variante P', 'Variante Q'], 'Descrizione prodotto 3', 15, 'Retailer 3');
     const productsListDiv = document.getElementById('productsList');
-    if (productsListDiv) {
+    const selectedProductsDiv = document.getElementById('selectedProducts');
+    if (productsListDiv && selectedProductsDiv) {
         [product1, product2, product3].forEach(product => {
-            const productCard = createProductCard(product);
+            const productCard = createProductCard(product, selectedProductsDiv);
             productsListDiv.appendChild(productCard);
         });
     }
     else {
-        console.error("Elemento 'productsList' non trovato.");
+        console.error("Elemento 'productsList' o 'selectedProducts' non trovato.");
     }
 });
-function createProductCard(product) {
+function createProductCard(product, selectedProductsDiv) {
     const card = document.createElement('div');
     card.classList.add('product-card');
     const checkbox = document.createElement('input');
@@ -22,6 +23,7 @@ function createProductCard(product) {
     const productName = document.createElement('span');
     productName.textContent = product.name;
     const variantSelect = document.createElement('select');
+    variantSelect.id = `variantSelect_${product.code}`; // Aggiungi un id univoco
     product.variants.forEach(variant => {
         const option = document.createElement('option');
         option.value = variant;
@@ -38,37 +40,50 @@ function createProductCard(product) {
     card.appendChild(quantityInput);
     checkbox.addEventListener('change', () => {
         if (checkbox.checked) {
-            addToSelectedProducts(product);
+            const quantity = +quantityInput.value;
+            addToSelectedProducts(product, quantity, selectedProductsDiv);
         }
         else {
-            removeFromSelectedProducts(product);
+            removeFromSelectedProducts(product, selectedProductsDiv);
         }
     });
     return card;
 }
-function addToSelectedProducts(product) {
-    const selectedProductsDiv = document.getElementById('selectedProducts');
-    if (selectedProductsDiv) {
-        const productItem = document.createElement('div');
-        productItem.textContent = product.name;
-        selectedProductsDiv.appendChild(productItem);
-    }
-    else {
-        console.error("Elemento 'selectedProducts' non trovato.");
-    }
+function addToSelectedProducts(product, quantity, selectedProductsDiv) {
+    const productItem = document.createElement('div');
+    productItem.classList.add('selected-product-item');
+    const quantitySpan = document.createElement('span');
+    quantitySpan.textContent = `${quantity}x`;
+    const productInfoSpan = document.createElement('span');
+    productInfoSpan.textContent = `${product.name} - ${getSelectedVariant(product)}`;
+    const removeButton = document.createElement('button');
+    removeButton.textContent = 'x';
+    removeButton.addEventListener('click', () => {
+        removeProductFromSelected(product, productItem, selectedProductsDiv);
+    });
+    productItem.appendChild(quantitySpan);
+    productItem.appendChild(productInfoSpan);
+    productItem.appendChild(removeButton);
+    selectedProductsDiv.appendChild(productItem);
 }
-function removeFromSelectedProducts(product) {
-    const selectedProductsDiv = document.getElementById('selectedProducts');
-    if (selectedProductsDiv) {
-        const productItems = selectedProductsDiv.getElementsByTagName('div');
-        for (const item of productItems) {
-            if (item.textContent === product.name) {
-                selectedProductsDiv.removeChild(item);
-                break;
-            }
+function removeFromSelectedProducts(product, selectedProductsDiv) {
+    const productItems = selectedProductsDiv.getElementsByClassName('selected-product-item');
+    for (const item of productItems) {
+        const quantitySpan = item.querySelector('span');
+        if (quantitySpan && quantitySpan.textContent && quantitySpan.textContent.includes(product.name)) {
+            selectedProductsDiv.removeChild(item);
+            break;
         }
     }
-    else {
-        console.error("Elemento 'selectedProducts' non trovato.");
+}
+function removeProductFromSelected(product, productItem, selectedProductsDiv) {
+    selectedProductsDiv.removeChild(productItem);
+}
+function getSelectedVariant(product) {
+    const variantSelect = document.getElementById(`variantSelect_${product.code}`);
+    if (variantSelect && variantSelect.options.length > 0) {
+        const selectedOption = variantSelect.options[variantSelect.selectedIndex];
+        return selectedOption.value;
     }
+    return '';
 }
--- app.ts
+++ app.ts
@@ -1,4 +1,4 @@
-import {Product} from './classi.js';
+import { Product } from './classi.js';
 
 document.addEventListener('DOMContentLoaded', () => {
 
@@ -7,18 +7,19 @@ document.addEventListener('DOMContentLoaded', () => {
   const product3 = new Product('3', 'Prodotto 3', ['Variante P', 'Variante Q'], 'Descrizione prodotto 3', 15, 'Retailer 3');
 
   const productsListDiv = document.getElementById('productsList');
+  const selectedProductsDiv = document.getElementById('selectedProducts');
 
-  if (productsListDiv) {
+  if (productsListDiv && selectedProductsDiv) {
     [product1, product2, product3].forEach(product => {
-      const productCard = createProductCard(product);
+      const productCard = createProductCard(product, selectedProductsDiv);
       productsListDiv.appendChild(productCard);
     });
   } else {
-    console.error("Elemento 'productsList' non trovato.");
+    console.error("Elemento 'productsList' o 'selectedProducts' non trovato.");
   }
 });
 
-function createProductCard(product: Product): HTMLDivElement {
+function createProductCard(product: Product, selectedProductsDiv: HTMLElement): HTMLDivElement {
   const card = document.createElement('div');
   card.classList.add('product-card');
 
@@ -29,6 +30,7 @@ function createProductCard(product: Product): HTMLDivElement {
   productName.textContent = product.name;
 
   const variantSelect = document.createElement('select');
+  variantSelect.id = `variantSelect_${product.code}`; // Aggiungi un id univoco
   product.variants.forEach(variant => {
     const option = document.createElement('option');
     option.value = variant;
@@ -48,40 +50,62 @@ function createProductCard(product: Product): HTMLDivElement {
 
   checkbox.addEventListener('change', () => {
     if (checkbox.checked) {
-      addToSelectedProducts(product);
+      const quantity = +quantityInput.value;
+      addToSelectedProducts(product, quantity, selectedProductsDiv);
     } else {
-      removeFromSelectedProducts(product);
+      removeFromSelectedProducts(product, selectedProductsDiv);
     }
   });
 
   return card;
 }
 
-function addToSelectedProducts(product: Product): void {
-  const selectedProductsDiv = document.getElementById('selectedProducts');
+function addToSelectedProducts(product: Product, quantity: number, selectedProductsDiv: HTMLElement): void {
+  const productItem = document.createElement('div');
+  productItem.classList.add('selected-product-item');
 
-  if (selectedProductsDiv) {
-    const productItem = document.createElement('div');
-    productItem.textContent = product.name;
-    selectedProductsDiv.appendChild(productItem);
-  } else {
-    console.error("Elemento 'selectedProducts' non trovato.");
+  const quantitySpan = document.createElement('span');
+  quantitySpan.textContent = `${quantity}x`;
+
+  const productInfoSpan = document.createElement('span');
+  productInfoSpan.textContent = `${product.name} - ${getSelectedVariant(product)}`;
+
+  const removeButton = document.createElement('button');
+  removeButton.textContent = 'x';
+  removeButton.addEventListener('click', () => {
+    removeProductFromSelected(product, productItem, selectedProductsDiv);
+  });
+
+  productItem.appendChild(quantitySpan);
+  productItem.appendChild(productInfoSpan);
+  productItem.appendChild(removeButton);
+
+  selectedProductsDiv.appendChild(productItem);
+}
+
+function removeFromSelectedProducts(product: Product, selectedProductsDiv: HTMLElement): void {
+  const productItems = selectedProductsDiv.getElementsByClassName('selected-product-item');
+
+  for (const item of productItems) {
+    const quantitySpan = item.querySelector('span');
+    if (quantitySpan && quantitySpan.textContent && quantitySpan.textContent.includes(product.name)) {
+      selectedProductsDiv.removeChild(item);
+      break;
+    }
   }
 }
 
-function removeFromSelectedProducts(product: Product): void {
-  const selectedProductsDiv = document.getElementById('selectedProducts');
+function removeProductFromSelected(product: Product, productItem: HTMLDivElement, selectedProductsDiv: HTMLElement): void {
+  selectedProductsDiv.removeChild(productItem);
+}
 
-  if (selectedProductsDiv) {
-    const productItems = selectedProductsDiv.getElementsByTagName('div');
+function getSelectedVariant(product: Product): string {
+  const variantSelect = document.getElementById(`variantSelect_${product.code}`) as HTMLSelectElement;
 
-    for (const item of productItems) {
-      if (item.textContent === product.name) {
-        selectedProductsDiv.removeChild(item);
-        break;
-      }
-    }
-  } else {
-    console.error("Elemento 'selectedProducts' non trovato.");
+  if (variantSelect && variantSelect.options.length > 0) {
+    const selectedOption = variantSelect.options[variantSelect.selectedIndex];
+    return selectedOption.value;
   }
+
+  return '';
 }
